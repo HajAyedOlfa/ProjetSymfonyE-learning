a92d77ca42031e56597a8fd82b684f5db01ad915
olfa <hajayedolfa@gmail.com>
2020-12-15 17:48:21 +0100

    Gérer les matières

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use App\Repository\MatiereRepository;
 
 class IndexController extends AbstractController
 {
@@ -13,11 +14,21 @@ class IndexController extends AbstractController
      * @Route("/index", name="index")
      * @IsGranted("ROLE_USER")
      */
-    public function index(): Response
+    public function index(MatiereRepository $matiereRepository): Response
     {
 
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
+            'matieres' => $matiereRepository->findAll(),
         ]);
     }
+
+
+
+
+
+
+
+
+
 }
